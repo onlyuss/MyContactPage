3314b247bd582ea0adf6775f939cdc8269ed9845
whtsp_ <hifuruhirz@gmail.com>
2026-06-03 16:00:33 +0700

    Update config.js

---
+++ config.js
@@ -0,0 +1,11 @@
+const config = {
+    profileImage: "profil.jpg",
+
+    backgrounds: [
+        "bg1.jpg",
+        "bg2.jpg"
+    ],
+
+    whatsappLink: "https://example.com",
+    telegramLink: "https://example.com"
+};
